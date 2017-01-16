b33fd3db9e6d5927456d98f197958c6990431178
albertocastano <bertocast@gmail.com>
2017-01-16 09:36:54 +0100

    Change the implementation of KLR

--- quantification/utils/models.py
+++ quantification/utils/models.py
@@ -1,142 +1,258 @@
-from copy import deepcopy
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 12 13:34:28 2013
 
+Least Squares Probabilistic Classifier
+
+Reference:
+Sugiyama, M. 
+"Superfast-trainable multi-class probabilistic classifier by 
+least-squares posterior fitting", IEICE Transactions on Information and
+Systems, vol.E93-D, no.10, pp.2690-2701, 2010. 
+http://sugiyama-www.cs.titech.ac.jp/~sugi/2010/LSPC.pdf
+
+@author: John Quinn <jquinn@cit.ac.ug>
+"""
+
+from sklearn import metrics, base, neighbors
 import numpy as np
-from sklearn.base import BaseEstimator, ClassifierMixin
-from sklearn.model_selection import cross_val_score
-from sklearn.utils import check_X_y
-import multiprocessing
-
-from quantification.classify_and_count.base import BaseMulticlassClassifyAndCount
 
 
-def sigmoid(z):
-    return 1 / (1 + np.exp(-z))
+def median_kneighbour_distance(X, k=5):
+    """Calculate the median distance between a set of random datapoints and
+    their kth nearest neighbours. This is a heuristic for setting the
+    kernel length scale."""
+    N_all = X.shape[0]
+    N_subset = min(N_all, 2000)
+    sample_idx_train = np.random.permutation(N_all)[:N_subset]
+    nn = neighbors.NearestNeighbors(k, warn_on_equidistant=False)
+    nn.fit(X[sample_idx_train, :])
+    d, idx = nn.kneighbors(X[sample_idx_train, :])
+    return np.median(d[:, -1])
 
 
-class KLR(BaseEstimator, ClassifierMixin):
-    def __init__(self, p=5, gamma=1, kernel='rbf', pos_class_weight=None):
-        self.p = p
-        self.gamma = gamma
-        self.kernel = kernel
-        self.pos_class_weight = pos_class_weight
+def pair_distance_centiles(X, centiles, max_pairs=2000):
+    """Helper function to find the centiles of pairwise distances in a
+    dataset. This can be useful information to help choose candidates for the
+    kernel parameter sigma.
 
-    def fit(self, X, y):
-        X, y = check_X_y(X, y, accept_sparse='csr', dtype=np.float64,
-                         order="C")
-        self.classes_ = np.unique(y)
-        n_samples, n_features = X.shape
+    Parameters
+    ----------
 
-        counts = np.bincount(y)
-        self.prior_ = counts[1] / float(len(y))
+    X : array 
+        Should have dimension N times d (rows are examples, columns are data
+        dimensions)
+    centiles : array
+        List of centiles to calculate, where all values are between 0 and 100
+    max_pairs : int
+        Maximum number of random pairs to evaluate.
 
-        self.weights, self.bias = self._get_weights(n_features)
-        Q = self._get_kernel(X)
+    Returns
+    -------
+    c : array
+        Calculated centiles of pairwise distances, of the same size as the 
+        input parameter "centiles".
+    """
+    N = X.shape[0]
+    n_pairs = min(max_pairs, N)
+    n_centiles = len(centiles)
+    randorder1 = np.random.permutation(N)
+    randorder2 = np.random.permutation(N)
 
-        pos_class_weight = self.pos_class_weight
-        if pos_class_weight == 'balanced':
-            counts = np.bincount(y)
-            pos_class_weight = counts[0] / float(counts[1])
+    dists = np.zeros(n_pairs)
 
-        y_ = deepcopy(y)
-        y_[y_==self.classes_[1]] = y_[y_==self.classes_[1]] * pos_class_weight
+    for i in range(n_pairs):
+        pairdiff = X[randorder1[i], :] - X[randorder2[i], :]
+        dists[i] = np.dot(pairdiff.T, pairdiff)
+    dists.sort()
 
-        self.beta, self.b = self.kernel_trick(Q, y_)
-        return self
+    out = np.zeros(n_centiles)
+    for i in range(n_centiles):
+        out[i] = dists[int(n_pairs * centiles[i] / 100.)]
 
-    def kernel_trick(self, Q, y):
-        s = 4.0 / self.gamma
-        f = np.dot(4.0 * Q.T, y - 0.5)
-        B = np.sum(Q, axis=0)
-        V = np.dot(Q.T, Q)
-        C = np.dot(Q, np.linalg.pinv(V))
-        C = np.sum(C, axis=0)
-        V[np.diag_indices_from(V)] += s
-        V_inv = np.linalg.pinv(V)
-        Z = np.dot(V_inv, f)
-        S_i = 1 / np.dot(np.dot(C, V_inv).T, B)
-        b = np.dot(S_i * C, Z)
-        beta = Z - np.dot(V_inv, B) * b
+    return np.sqrt(out)
 
-        return beta, b
 
-    def predict_proba(self, X):
-        Q = self._get_kernel(X)
-        p = np.dot(Q, self.beta) + self.b
-        prob = sigmoid(p)
-        return np.column_stack((1 - prob, prob))
+class LSPC(base.BaseEstimator):
+    """Least Squares Probabilistic Classifier.
+
+    Parameters
+    ----------
+    n_kernels_max : int, optional (default 5000)
+        Maximum number of kernel basis centres to use.
+    kernel_pos : optional
+        The positions of the kernel centres can be specified. Default is to
+        select them as a random subset of the training data.
+    basis_set: 'full' or 'classwise' (default: 'classwise')
+        If set to 'full', use training points from all classes as the kernel
+        basis set. If 'classwise', use a separate set of kernel bases for
+        each class, using training points from only that class (much faster).
+
+    Example
+    -------
+
+    >>> import lspc
+    >>> from sklearn import datasets, cross_validation, metrics, grid_search 
+    >>> iris = datasets.load_iris()
+    >>> X = iris.data
+    >>> y = iris.target
+    >>> sigma_candidates = lspc.pair_distance_centiles(X, 
+                            centiles=[10, 50, 90]) 
+    >>> rho_candidates = [.01, .1, .5, 1.]    
+    >>> param_grid = {'sigma':sigma_candidates, 'rho':rho_candidates}
+    >>> cv_folds = 5
+    >>> clf = grid_search.GridSearchCV(lspc.LSPC(),
+    ...                                param_grid, cv=cv_folds,
+    ...                                score_func=metrics.accuracy_score,
+    ...                                n_jobs=-1)  
+    >>> kf = cross_validation.KFold(n=X.shape[0], n_folds=3, shuffle=True)
+    >>> predictions= np.zeros(X.shape[0])
+    >>> for train_index, test_index in kf:
+    ...     clf.fit(X[train_index,:], y[train_index])
+    ...     predictions[test_index] = clf.predict(X[test_index])
+    """
+
+    def __init__(self, n_kernels_max=7000, kernel_pos=None,
+                 basis_set='classwise', sigma=None, gamma=None, rho=None):
+        self.n_kernels_max = n_kernels_max
+        self.kernel_pos = kernel_pos
+        self.rho = None
+        self.theta = None
+        self.basis_classes = None
+        self.basis_set = basis_set
+        self.sigma = None
+        self.gamma = None
+
+    def fit(self, X, y, rho=.01, sigma=None, gamma=None):
+        """Fit the LSPC model according to the given training data.
+
+        Parameters
+        ----------
+        X : array, shape = [n_samples, n_features]
+            Training vectors, where n_samples is the number of samples
+            and n_features is the number of features.
+
+        y : array-like, shape = [n_samples]
+            Target class labels
+        sigma : float, optional
+            Kernel scale parameter.
+        gamma : float, optional
+            An alternative way of specifying the kernel scale parameter,
+            for compatibility with SVM notation. sigma = 1/sqrt(gamma)
+        rho : float, optional
+            Regularization parameter.
+        """
+
+        self.classes = np.unique(y)
+        self.classes.sort()
+        self.n_classes = len(self.classes)
+
+        N = X.shape[0]
+
+        # If no kernel parameters have been specified, try to choose some
+        # reasonable defaults.      
+        if sigma:
+            self.sigma = sigma
+            self.gamma = sigma ** -2
+        if gamma:
+            self.gamma = gamma
+            self.sigma = gamma ** -.5
+        if not self.sigma:
+            neighbour_dist = median_kneighbour_distance(X)
+            # median_dist = pair_distance_centiles(X,[50])[0]
+            self.sigma = neighbour_dist
+        if not self.gamma:
+            self.gamma = self.sigma ** -2
+        if not self.rho:
+            self.rho = 0.1
+
+        # choose kernel basis centres
+        if self.kernel_pos == None:
+            B = min(self.n_kernels_max, N)
+            kernel_idx = np.random.permutation(N)
+            self.kernel_pos = X[kernel_idx[:B], :]
+            self.basis_classes = y[kernel_idx[:B]]
+        else:
+            B = self.kernel_pos.shape[0]
+
+        # fit coefficients
+        Phi = metrics.pairwise.rbf_kernel(X, self.kernel_pos, self.gamma)
+        theta = {}
+        '''
+        for c in self.classes:
+            m = (y==c).astype(int)
+            if self.basis_set=='full':
+                kidx = np.ones(Phi.shape[1]).astype('bool')
+            else:
+                kidx = self.basis_classes==c
+            theta[c] = np.linalg.inv(np.dot(Phi[:,kidx].T,Phi[:,kidx]) 
+                        + self.rho*np.eye(sum(kidx)))
+            theta[c] = np.dot(theta[c],np.dot(Phi[:,kidx].T,m))   
+
+        self.theta = theta
+        '''
+
+        if self.basis_set == 'full':
+            inv_part = np.linalg.inv(np.dot(Phi.T, Phi)
+                                     + self.rho * np.eye(B))
+
+        for c in self.classes:
+            m = (y == c).astype(int)
+            if self.basis_set == 'full':
+                kidx = np.ones(Phi.shape[1]).astype('bool')
+            else:
+                kidx = self.basis_classes == c
+                inv_part = np.linalg.inv(np.dot(Phi[:, kidx].T, Phi[:, kidx])
+                                         + self.rho * np.eye(sum(kidx)))
+
+            theta[c] = np.dot(inv_part, np.dot(Phi[:, kidx].T, m))
+
+        self.theta = theta
 
     def predict(self, X):
-        prob = self.predict_proba(X)
-        preds = (prob[:, 1] > self.prior_).astype(int)
-        preds[preds==0] = self.classes_[0]
-        preds[preds==1] = self.classes_[1]
-        return preds
+        """Perform classification on samples in X, and return most likely
+        classes for each test instance.
 
-    def _get_weights(self, n_feat):
-        if self.kernel in ['sigmoid', 'HypTan', 'Fourier', 'HardLimit']:
-            w = np.random.uniform(-1 / np.sqrt(n_feat), 1 / np.sqrt(n_feat), (self.p, n_feat))
-            b = np.apply_along_axis(lambda x: np.random.uniform(min(x), abs(max(x))), 1, w)
-        elif self.kernel in ['rbf', 'MultiQuad', 'hybrid']:
-            w = np.random.uniform(-1, 1, (self.p, n_feat))
-            b = np.random.uniform(-1, 1, self.p)
+        Parameters
+        ----------
+        X : array, shape = [n_samples, n_features]
 
-        return w, b
+        Returns
+        -------
+        y_pred : array, shape = [n_samples]
+            Class labels for samples in X.
+        """
+        predictions_proba = self.predict_proba(X)
+        predictions = []
+        for i in range(X.shape[0]):
+            predictions.append(self.classes[predictions_proba[i, :].argmax()])
+        return predictions
 
-    def _get_kernel(self, X):
+    def predict_proba(self, X):
+        """Compute probabilities of possible outcomes for samples in X.
 
-        if self.kernel == 'sigmoid':
-            W = np.column_stack((self.weights, self.bias))
-            X_ = np.column_stack((X, np.ones(X.shape[0])))
-            Q = np.dot(X_, W.T)
-            Q = sigmoid(Q)
-            return Q
+        Parameters
+        ----------
+        X : array, shape = [n_samples, n_features]
 
-        elif self.kernel == 'rbf':
-            Q = np.empty((X.shape[0], self.p))
-            for i in range(self.p):
-                Q[:, i] = np.apply_along_axis(lambda x: self.bias[i] * sum((x - self.weights[i,]) ** 2), 1, X)
-            return Q
-
-    def score(self, X, y, sample_weight=None):
-        from sklearn.metrics import confusion_matrix
-
-        cm = confusion_matrix(y, self.predict(X), sample_weight=sample_weight)
-
-        fpr = cm[0, 1] / float(cm[0, 1] + cm[0, 0])
-        tpr = cm[1, 1] / float(cm[1, 1] + cm[1, 0])
-
-        return np.sqrt((1 - fpr) * tpr)
-
-
-if __name__ == '__main__':
-    import pandas as pd
-    from sklearn.preprocessing import LabelEncoder
-    from sklearn.datasets.base import Bunch
-    def load_plankton_file(path, sample_col="Sample", target_col="class"):
-        data_file = pd.read_csv(path, delimiter=' ')
-        le = LabelEncoder()
-        data_file[target_col] = le.fit_transform(data_file[target_col])
-        data = data_file.groupby(sample_col)
-        target = [sample[1].values for sample in data[target_col]]
-        features = [sample[1].drop([sample_col, target_col], axis=1, inplace=False).values for sample in data]
-        return Bunch(data=features, target=target,
-                     target_names=le.classes_), le
-
-    from sklearn.datasets import load_breast_cancer
-    from sklearn.metrics import confusion_matrix
-
-    pl, le = load_plankton_file('../../examples/plancton.csv')
-    X = np.concatenate(pl.data)
-    y = np.concatenate(pl.target)
-
-
-    pos_ind = np.where(y==1)[0]
-    neg_ind = np.where(y==0)[0]
-    pos_ind = pos_ind[:20]
-    ind = np.concatenate((pos_ind, neg_ind))
-    X = X[ind, :]
-    y = y[ind]
-
-    klr = KLR(p=30)
-    klr.fit(X, y)
-    print confusion_matrix(y_true=y, y_pred=klr.predict(X))
+        Returns
+        -------
+        Y_prob : array, shape = [n_samples, n_classes]
+            Returns the probability of the sample for each class in
+            the model.
+        """
+        Phi = metrics.pairwise.rbf_kernel(X, self.kernel_pos, self.gamma)
+        N = X.shape[0]
+        predictions = np.zeros((N, self.n_classes))
+        for i in range(N):
+            post = np.zeros(self.n_classes)
+            for c in range(self.n_classes):
+                if self.basis_set == 'full':
+                    kidx = np.ones(Phi.shape[1]).astype('bool')
+                else:
+                    kidx = self.basis_classes == self.classes[c]
+                post[c] = max(0, np.dot(self.theta[self.classes[c]].T,
+                                        Phi[i, kidx]))
+            post = post / sum(post)
+            predictions[i, :] = post
+        return predictions      
