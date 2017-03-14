a2f235616db99f144074a2fe57ab55b70363f5ed
albertocastano <bertocast@gmail.com>
2017-03-14 09:26:54 +0100

    Include a debugging message

--- quantification/classify_and_count/ensemble.py
+++ quantification/classify_and_count/ensemble.py
@@ -49,10 +49,12 @@ class EnsembleBinaryCC(BaseEnsembleCCModel):
         return self
 
     def _performance(self, qnf, n, X, y):
+        print len(X)
+        print len(y)
+        print n
         X_val = np.concatenate(X[:n] + X[(n + 1):])
         y_val = np.concatenate(y[:n] + y[(n + 1):])
         print len(X_val)
-        print [X__.shape for X__ in X_val]
         cm = []
         for X_, y_ in zip(X_val, y_val):
             cm.append(confusion_matrix(y_, qnf.estimator_.predict(X_)))
