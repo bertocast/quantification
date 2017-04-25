2f6537c7153214fdf50996c9f339533fab2463fe
albertocastano <bertocast@gmail.com>
2017-04-25 10:12:04 +0200

    First start with ordinal quantification

---
+++ quantification/ordinal/base.py
@@ -0,0 +1,21 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+
+class OQT():
+
+    def fit(self, X, y):
+
+        n_class = len(np.unique(y))
+
+        for n in range(n_class):
+            pos_class = y[:n]
+
+            mask = (y == pos_class)
+            y_bin = np.ones(y.shape, dtype=np.int)
+            y_bin[~mask] = 0
+
+            clf = LinearRegression()
+
+    def predict(self, X):
+        pass
