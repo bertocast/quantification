79b02a64b7d33c7aae0356a27b6a03efe4551a4e
albertocastano <bertocast@gmail.com>
2018-04-19 12:55:23 +0200

    Fix a bug where a classifier was retrained over different data.

--- quantification/metrics/model_score.py
+++ quantification/metrics/model_score.py
@@ -1,13 +1,15 @@
 from __future__ import print_function
 import numpy as np
+from copy import deepcopy
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import StratifiedKFold
 
 
 def cv_confusion_matrix(clf, X, y, folds=50, verbose=False):
+    clf_c = deepcopy(clf)
     skf = StratifiedKFold(n_splits=folds)
     cv_iter = skf.split(X, y)
-    n_classes = len(clf.classes_)
+    n_classes = len(clf_c.classes_)
     cms = []
 
     if verbose:
@@ -16,7 +18,7 @@ def cv_confusion_matrix(clf, X, y, folds=50, verbose=False):
     for n, (train, test) in enumerate(cv_iter):
         if verbose:
             print("\t{}/{}".format(n+1, folds))
-        clf.fit(X[train,], y[train])
-        cm = confusion_matrix(y[test], clf.predict(X[test]), labels=clf.classes_)
+            clf_c.fit(X[train,], y[train])
+        cm = confusion_matrix(y[test], clf_c.predict(X[test]), labels=clf_c.classes_)
         cms.append(cm)
     return np.array(cms)
